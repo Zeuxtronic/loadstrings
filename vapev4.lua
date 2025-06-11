a8f136704368b2382fdd4a4b6ade0af02c4782fb
Zeuxtronic <zeuxtronic@gmail.com>
2025-06-11 18:03:35 +0300

    Update vapev4.lua

--- vapev4.lua
+++ vapev4.lua
@@ -1,5 +1,5 @@
 -- literally just vapev4 but queueteleport because i dunno doesnt work for me if it exists
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 queueteleport([[
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeuxtronic/loadstrings/refs/heads/main/vapev4.lua", true))()
 ]])
